f59a8c79ffe7ef5ed09c75127188af6c1ef37a97
ValeriyaSmolova <100532617+ValeriyaSmolova@users.noreply.github.com>
2022-05-14 23:22:18 +0300

    Add files via upload

--- js/pricing-modal.js
+++ js/pricing-modal.js
@@ -1,21 +1,25 @@
-const form = document.getElementsByClassName('pricing-modal-form-wrapper')[0];
-const userName = document.getElementById("name");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
+const form = document.querySelector('.pricing-modal-form-wrapper');
+const userName = document.getElementById("pricename");
+const email = document.getElementById("emailll");
+const phone = document.getElementById("pricephone");
 const modal = document.getElementById("pricing-modal-wrapper");
 const closeModal = document.getElementById("pricing-modal-close");
 var openModalBtn;
-const orderLabel = document.getElementById("order-accept");
+const priceBtn = document.querySelectorAll(".price__btn");
 
-const emailPattern = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+const orderLabel = document.getElementById("order-accept");
+const submitBtn = document.querySelector('.pricing-modal-btn');
+
+const emailPattern = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
 const BYPhonePattern = new RegExp(/^(\+375|80)(29|25|44|33)(\d{3})(\d{2})(\d{2})$/);
 
-window.onclick = (event) => {
-    if (event.target === modal)
-        modal.style.display = "none";
-}
 
-closeModal.onclick = () => modal.style.display = "none";
+closeModal.onclick = () => {
+    modal.style.display = "none";
+    document.body.style = "padding-right: 0"
+    document.body.classList.remove('lock');
+    
+    }
 
 const onOrderBtnClick = (event) => {
 
@@ -23,42 +27,60 @@ const onOrderBtnClick = (event) => {
     form.style.display = "flex";
     closeModal.style.display = "flex";
     orderLabel.style.display = "none";
-    openModalBtn = document.getElementById(event.target.id);
-
-    var userOrders = JSON.parse(window.localStorage.getItem('userOrders'));
-    if (userOrders !== null && userOrders.includes(openModalBtn.id)) {
-        form.style.display = "none";
-        orderLabel.style.display = "block";
-    }
-
+    
+    
 }
 
-const onFormChange = (event) => {
-    const submitBtn = document.getElementsByClassName('pricing-modal-btn')[0];
-    submitBtn.disabled = userName.value.length === 0 || !emailPattern.test(email.value) || !BYPhonePattern.test(phone.value); 
+form.addEventListener('change', () => {
+    !(userName.value.length === 0) && emailPattern.test(email.value) && BYPhonePattern.test(phone.value) ? submitBtn.disabled = false : submitBtn.disabled = true;
+})
+let target;
+submitBtn.addEventListener('click', (e) => {
+    form.reset();
+    e.preventDefault();
+   
+    form.style = 'display:none;'
+    showOrderModal();
+    closeModalFunc();
+    localStorage.setItem(target, true);
+});
+
+
+priceBtn.forEach(item => {
+    item.addEventListener('click', (e) => {
+        document.body.style = "padding-right: 15px"
+        document.body.classList.add('lock');
+        target = e.target.id;
+        clearTimeout(initial);
+        let targetId = localStorage.getItem(target);
+        
+        if(targetId === "true") {
+            form.style = 'display:none;'
+            showOrderModal();
+            closeModalFunc();
+        } else {
+            return null;
+        }
+    })
+})
+
+let initial;
+function closeModalFunc() {
+    
+    initial = setTimeout(() => {
+        document.body.style = "padding-right: 0"
+        document.body.classList.remove('lock');
+         orderLabel.style = "display: none";
+         closeModal.style = "display: none";
+        modal.style = "display:none";
+        
+    },5000);
+    
+    
 }
 
-const onFieldChange = (event) => {
-    var result = false;
-    switch (event.target.id) {
-        case "name" : result = event.target.value.length !== 0; break;
-        case "email" : result = emailPattern.test(event.target.value); break;
-        case "phone" : result = BYPhonePattern.test(event.target.value); break;
-    }
-    event.target.style.borderColor = result ? "#00897b" : "red";
-}
-
-const onFormSubmit = (event) => {
-
-    var userOrders = window.localStorage.getItem('userOrders');
-    userOrders = userOrders === null ? [] : JSON.parse(userOrders);
-    userOrders.push(openModalBtn.id);
-    window.localStorage.setItem('userOrders', JSON.stringify(userOrders));
-
-    event.target.style.display = "none";
-    closeModal.style.display = "none";
-    orderLabel.style.display = "block";
-    event.preventDefault();
-    setTimeout(() => { modal.style.display = "none" }, 5000);    
-
+function showOrderModal() {
+    
+    closeModal.style = "display: block"
+    orderLabel.style = "display: block";
 }
